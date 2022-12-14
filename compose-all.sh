74b9d746e47c397f672d3349ebf3bc9f582b618b
Dawid Wijata <dawid.wijata@hotmail.com>
2022-12-14 02:06:03 +0100

    Fix compose-all project paths

--- compose-all.sh
+++ compose-all.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 cd ..
-docker-compose -f services/Midas.Services.Authorization/docker-compose.yml up && \
-docker-compose -f services/Midas.Services.Users/docker-compose.yml up && \
-docker-compose -f services/Midas.Services.Families/docker-compose.yml up && \
-docker-compose -f services/Midas.Services.FileStorage/docker-compose.yml up && \
-docker-compose -f services/Midas.Services.Transactions/docker-compose.yml up && \
+docker-compose -f Midas.Services/Authorization/docker-compose.yml up && \
+docker-compose -f Midas.Services/Users/docker-compose.yml up && \
+docker-compose -f Midas.Services/Families/docker-compose.yml up && \
+docker-compose -f Midas.Services/FileStorage/docker-compose.yml up && \
+docker-compose -f Midas.Services/Transactions/docker-compose.yml up && \
 
-docker-compose -f frontends/Midas.Frontends.Authorization/docker-compose.yml up && \
-docker-compose -f frontends/Midas.Frontends.Container/docker-compose.yml up && \
-docker-compose -f frontends/Midas.Frontends.Navigation/docker-compose.yml up && \
-docker-compose -f frontends/Midas.Frontends.Users/docker-compose.yml up && \
-docker-compose -f frontends/Midas.Frontends.Families/docker-compose.yml up && \
-docker-compose -f frontends/Midas.Frontends.FileStorage/docker-compose.yml up && \
-docker-compose -f frontends/Midas.Frontends.Transactions/docker-compose.yml up
+docker-compose -f Midas.Frontends/Authorization/docker-compose.yml up && \
+docker-compose -f Midas.Frontends/Container/docker-compose.yml up && \
+docker-compose -f Midas.Frontends/Navigation/docker-compose.yml up && \
+docker-compose -f Midas.Frontends/Users/docker-compose.yml up && \
+docker-compose -f Midas.Frontends/Families/docker-compose.yml up && \
+docker-compose -f Midas.Frontends/FileStorage/docker-compose.yml up && \
+docker-compose -f Midas.Frontends/Transactions/docker-compose.yml up
 
