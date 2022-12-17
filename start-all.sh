ce420af7ee59b3ce756e2e4bd2d16f11cd5ce065
Dawid Wijata <dawid.wijata@hotmail.com>
2022-12-17 17:19:25 +0100

    Apply manual migrating scripts

--- start-all.sh
+++ start-all.sh
@@ -17,16 +17,16 @@ sleep 10s
 
 # Uncomment below if you need manual database updating
 
-#echo 'Migrating databases...'
-#dotnet ef database update --project Midas.Services/Users/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/Users/src/WebAPI/WebAPI.csproj --context Infrastructure.Data.UserDbContext --configuration Release
+echo 'Migrating databases...'
+dotnet ef database update --project Midas.Services/Users/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/Users/src/WebAPI/WebAPI.csproj --context Infrastructure.Data.UserDbContext --configuration Release
 
-#dotnet ef database update --project Midas.Services/Authorization/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/Authorizationsrc/WebAPI/WebAPI.csproj --context Infrastructure.Data.AuthorizationDbContext --configuration Release
+dotnet ef database update --project Midas.Services/Authorization/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/Authorization/src/WebAPI/WebAPI.csproj --context Infrastructure.Data.AuthorizationDbContext --configuration Release
 
-#dotnet ef database update --project Midas.Services/Families/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/Families/src/WebAPI/WebAPI.csproj --context Infrastructure.Data.FamilyDbContext --configuration Release
+dotnet ef database update --project Midas.Services/Families/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/Families/src/WebAPI/WebAPI.csproj --context Infrastructure.Data.FamilyDbContext --configuration Release
 
-#dotnet ef database update --project Midas.Services/FileStorage/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/FileStorage/src/WebAPI/WebAPI.csproj --context Infrastructure.Data.FileDbContext --configuration Release
+dotnet ef database update --project Midas.Services/FileStorage/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/FileStorage/src/WebAPI/WebAPI.csproj --context Infrastructure.Data.FileDbContext --configuration Release
 
-#dotnet ef database update --project Midas.Services/Transactions/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/Transactions/src/WebAPI/WebAPI.csproj --context Infrastructure.Data.TransactionDbContext --configuration Release
+dotnet ef database update --project Midas.Services/Transactions/src/Infrastructure/Infrastructure.csproj --startup-project Midas.Services/Transactions/src/WebAPI/WebAPI.csproj --context Infrastructure.Data.TransactionDbContext --configuration Release
 
 echo 'Running web services...'
 
